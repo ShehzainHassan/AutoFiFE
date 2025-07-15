d3685c1f38206ed83339056a35a46b4c5f2057bb
Shehzain Hassan <shehzainhassan@gmail.com>
2025-07-15 21:04:21 +0500

    Added redirect to auction page on search auction page

--- src/app/components/auction/search-auction/auction-card/auction-card.tsx
+++ src/app/components/auction/search-auction/auction-card/auction-card.tsx
@@ -4,11 +4,17 @@ import CarImage from "@/app/components/result-card/car-image/car-image";
 import ImageSrc from "@/assets/images/cars/Bentley-Arnage4.4.png";
 import classes from "../search-auction.module.css";
 import { CURRENCY } from "@/constants";
+import { useRouter } from "next/navigation";
 export default function SearchCard({ auction }: SearchCardProps) {
   const { hours, minutes, seconds } = useCountdown(auction.endUtc);
-
+  const router = useRouter();
+  const redirectToAuction = (auctionId: number) => {
+    router.push(`/auction/${auctionId}`);
+  };
   return (
-    <div className={classes.vehicleCard}>
+    <div
+      className={classes.vehicleCard}
+      onClick={() => redirectToAuction(auction.auctionId)}>
       <div className={classes.imageWrapper}>
         <CarImage src={ImageSrc} />
       </div>
