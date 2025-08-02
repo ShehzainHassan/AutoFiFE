6d192b22702852af946a29fb1944d5c2a042ed9e
Shehzain Hassan <shehzainhassan@gmail.com>
2025-08-02 12:43:32 +0500

    Fixed Build error

--- src/app/auction/page.tsx
+++ src/app/auction/page.tsx
@@ -1,12 +1,11 @@
 import { LiveActivity, Navbar } from "@/app/components";
-import { dropdownFilters } from "@/constants/auction";
 import Footer from "../components/Footer-Component/footer";
 import classes from "./page.module.css";
 export default function AuctionPage() {
   return (
     <div className={classes.container}>
       <Navbar backgroundColor="var(--color-gray600)" />
-      <LiveActivity dropdownFilters={dropdownFilters} />
+      <LiveActivity />
       <Footer />
     </div>
   );
